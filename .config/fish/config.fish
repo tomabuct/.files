bc09eb2e7cc5e25da88c1355c00b454055d4128f
Tom Abraham <tabraham@andrew.cmu.edu>
2013-04-07 22:01:54 -0400

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+### Aliases
+alias vi=vim
+alias gw='ssh -X greatwhite.ics.cs.cmu.edu'
+alias andrew='ssh -X unix.andrew.cmu.edu'
+alias ip=ipython
+alias ghc='ssh -X ghc23.ghc.andrew.cmu.edu'
+alias sml='rlwrap sml'
+
+## Git Aliases
+alias gs='git status'
+alias gco='git checkout'
+alias gl='git log'
+alias gau='git add -u'
+alias gd='git diff'
